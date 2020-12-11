2809a5d289a432fcc477c3c7c08463da31af2ce6
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-12 08:31:35 +0900

    簡単ログイン：メソッド定義

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
   def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRamdom.urlsafe_base64
+      # user.comfirmed_at = Time.now
   end
+  sign_in user
+  redirect_to root_path, notice: "ゲストユーザーとしてログインしました。"
 end
