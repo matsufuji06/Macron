415891b3189f4beda61fdab4db5ae37fecfb6d89
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-13 11:18:04 +0900

    日付選択の表示、form_forの記述とcreateアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to :root
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:food, :calorie, :carbo, :fat, :protein, :weight, :date).merge(user: current_user)
+  end
 end
