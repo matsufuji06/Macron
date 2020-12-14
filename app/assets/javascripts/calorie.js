615863da646a044ed3e5e4732629c5a227313058
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-14 21:30:28 +0900

    inputイベントを発火させ、入力された数値をコンソールに表示

--- app/assets/javascripts/calorie.js
+++ app/assets/javascripts/calorie.js
@@ -1,3 +1,11 @@
-const calculation = () => {
-  
-};
+window.addEventListener("load", function() {
+  const carbo = document.getElementsByClassName("form")[1];
+  const fat = document.getElementsByClassName("form")[2];
+  const protein = document.getElementsByClassName("form")[3];
+
+  const calculateCalorie = () => {
+    console.log(carbo.value);
+  };
+
+  carbo.addEventListener("input", calculateCalorie);
+});
