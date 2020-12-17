46377880c7c88cf22052aab06be43c482c2de3a7
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-18 07:08:47 +0900

    削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :root
+  end
 
   private
   def post_params
