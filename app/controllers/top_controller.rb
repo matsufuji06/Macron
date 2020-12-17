2cf8ab0cca5ae2f89f36f3d39381a302bad89eaf
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-18 07:12:33 +0900

    トップ画面で最新の５投稿のみ表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@ class TopController < ApplicationController
   def index
     @posts = Post.all.includes(:user)
     posts = @posts
-    @myposts = current_user.posts
+    @myposts = current_user.posts.order('created_at DESC').limit(5)
   end
 end
