d252a67b3ff11e386bafedab7a04bcf748d45c2c
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-28 20:51:30 +0900

    standardsコントローラーにcreateアクションを定義

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -2,4 +2,18 @@ class StandardsController < ApplicationController
   def new
     @standard = Standard.new
   end
+
+  def create
+    @standard = Standard.new(standard_params)
+    if @standard.save
+      redirect_to :root
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def standard_params
+    params.require(:standard).permit(:calorie, :carbo, :fat, :protein, :weight).merge(user: current_user)
+  end
 end
