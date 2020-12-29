11b5d47bca5332a39ba3d39c3448680c359f719c
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-30 08:32:02 +0900

    standardテーブルに”基礎代謝”など新カラム追加

--- app/assets/javascripts/standard.js
+++ app/assets/javascripts/standard.js
@@ -1,15 +1,24 @@
 window.addEventListener("load", function() {
+  const weightStandard = document.getElementsByClassName("standardForm")[0];
+  const idealweightStandard = document.getElementsByClassName("standardForm")[1];
   const carboStandard = document.getElementsByClassName("standardForm")[2];
   const fatStandard = document.getElementsByClassName("standardForm")[3];
   const proteinStandard = document.getElementsByClassName("standardForm")[4];
   const calorieStandard = document.getElementsByClassName("standardForm")[5];
 
-  const calculateCalorie = () => {
-    const sum = carboStandard.value*4 + fatStandard.value*9 + proteinStandard.value*4;
-    calorieStandard.value = sum;
-  };
+  const calculateStandard = () => {
+    const gap = weightStandard.value - idealweightStandard.value
+    
+    const calculateCalorie = () => {
+      const sum = carboStandard.value*4 + fatStandard.value*9 + proteinStandard.value*4;
+      calorieStandard.value = sum;
+    };
 
-  carboStandard.addEventListener("input", calculateCalorie);
-  fatStandard.addEventListener("input", calculateCalorie);
-  proteinStandard.addEventListener("input", calculateCalorie);
+    carboStandard.addEventListener("input", calculateCalorie);
+    fatStandard.addEventListener("input", calculateCalorie);
+    proteinStandard.addEventListener("input", calculateCalorie);
+  }
+
+  weightStandard.addEventListener("input", calculateStandard);
+  idealweightStandard.addEventListener("input", calculateStandard);
 });
--- db/migrate/20201227104157_create_standards.rb
+++ db/migrate/20201227104157_create_standards.rb
@@ -1,13 +1,17 @@
 class CreateStandards < ActiveRecord::Migration[5.0]
   def change
     create_table :standards do |t|
-      t.references :user,         null: false, foreign_key: true
-      t.decimal    :calorie,      null: false
-      t.decimal    :carbo,        null: false
-      t.decimal    :fat,          null: false
-      t.decimal    :protein,      null: false
-      t.decimal    :weight,       null: false
-      t.decimal    :idealweight,  null: false
+      t.references :user,             null: false, foreign_key: true
+      t.decimal    :calorie,          null: false
+      t.decimal    :carbo,            null: false
+      t.decimal    :fat,              null: false
+      t.decimal    :protein,          null: false
+      t.decimal    :weight,           null: false
+      t.decimal    :idealweight,      null: false
+      t.decimal    :metabolism,       null: false
+      t.decimal    :basalmetabolism,  null: false
+      t.integer    :age,              null: false, default: 0
+      t.integer    :workinglevel,     null: false, default: 0
       t.timestamps
     end
   end
