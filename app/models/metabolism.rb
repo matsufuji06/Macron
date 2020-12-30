3a7c0321d56fed93ae270b9e53f69280cdedc4e4
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-30 09:37:51 +0900

    seedではなく、active_hashを導入→代謝のデータを格納

---
+++ app/models/metabolism.rb
@@ -0,0 +1,26 @@
+class Metabolism < ActiveHash::Base
+  self.data = [
+    { sex: 0, age: 0, basalmetabolism: 700 },
+    { sex: 0, age: 1, basalmetabolism: 900 },
+    { sex: 0, age: 2, basalmetabolism: 980 },
+    { sex: 0, age: 3, basalmetabolism: 1140 },
+    { sex: 0, age: 4, basalmetabolism: 1330 },
+    { sex: 0, age: 5, basalmetabolism: 1520 },
+    { sex: 0, age: 6, basalmetabolism: 1610 },
+    { sex: 0, age: 7, basalmetabolism: 1520 },
+    { sex: 0, age: 8, basalmetabolism: 1530 },
+    { sex: 0, age: 9, basalmetabolism: 1400 },
+    { sex: 0, age: 10, basalmetabolism: 1290 },
+    { sex: 1, age: 0, basalmetabolism: 660 },
+    { sex: 1, age: 1, basalmetabolism: 840 },
+    { sex: 1, age: 2, basalmetabolism: 920 },
+    { sex: 1, age: 3, basalmetabolism: 1050 },
+    { sex: 1, age: 4, basalmetabolism: 1260 },
+    { sex: 1, age: 5, basalmetabolism: 1410 },
+    { sex: 1, age: 6, basalmetabolism: 1310 },
+    { sex: 1, age: 7, basalmetabolism: 1110 },
+    { sex: 1, age: 8, basalmetabolism: 1150 },
+    { sex: 1, age: 9, basalmetabolism: 1110 },
+    { sex: 1, age: 10, basalmetabolism: 1020 },
+  ]
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,7 @@
-Standard.create!(
-  [
-    {
-      age: 0
-      basalmetabolism: 700
-    },
-    {
-      age: 1
-      basalmetabolism: 900
-    },
-    {
-      age: 2
-      basalmetabolism: 980
-    },
-    {
-      age: 3
-      basalmetabolism: 1140
-    },
-    {
-      age: 4
-      basalmetabolism: 1330
-    },
-    {
-      age: 5
-      basalmetabolism: 1520
-    },
-    {
-      age: 6
-      basalmetabolism: 1610
-    },
-    {
-      age: 7
-      basalmetabolism: 1520
-    },
-    {
-      age: 8
-      basalmetabolism: 1530
-    },
-    {
-      age: 9
-      basalmetabolism: 1400
-    },
-    {
-      age: 10
-      basalmetabolism: 1290
-    },
-  ]
-)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
