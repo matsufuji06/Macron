af0bedce4731e9d243a23ea9755b43404ed3f15f
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-08 07:43:19 +0900

    日本語設定でuser_spec.rbの"nameがない場合は登録できないこと"をパスさせた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-
+      user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
     end
   end
 end
