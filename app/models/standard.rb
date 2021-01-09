d1256c812a392a5be9d6fb9be4925983245782de
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-09 15:10:33 +0900

    エラーメッセージのスタイリングとStandardモデルのバリデーション記載

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,4 +1,16 @@
 class Standard < ApplicationRecord
   # リレーションを定義
   belongs_to :user
+
+  # バリデーションを記述
+  standard_number = /\A[0-9]+\z/
+  validates :calorie, presence: true, format: {with: standard_number}
+  validates :carbo, presence: true, format: {with: standard_number}
+  validates :fat, presence: true, format: {with: standard_number}
+  validates :protein, presence: true, format: {with: standard_number}
+  validates :weight, presence: true, format: {with: standard_number}
+  validates :idealweight, presence: true, format: {with: standard_number}
+  validates :metabolism, presence: true, format: {with: standard_number}
+  validates :age, presence: true
+  validates :sex, presence: true
 end
