755dd43aa95865889a01f01d2a460ea1f4cd2a40
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-19 22:12:02 +0900

    "emailがない場合は登録できないこと"のユーザーモデル単体テストを追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ describe User do
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
+    it "emailがない場合は登録できないこと" do
+      user = User.new(name: "manabu", email: nil, password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
   end
 end
