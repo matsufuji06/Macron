9ffba43b0c2da2a0eb93b980365a14adf77d9443
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-20 15:16:50 +0900

    "重複したemailが存在する場合登録できないこと"のテストを通した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,13 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
+    it "重複したemailが存在する場合登録できないこと" do
+      #はじめにユーザーを登録
+      user = create(:user)
+      #先に登録したユーザーと同じemailの値を持つユーザーのインスタンスを作成
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
   end
 end
