376a75b517c206d76f5dea90097d341b00d62642
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-20 15:12:41 +0900

    "name、email、passwordとpassword_confirmationが存在すれば登録できること"のパスを通した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "name、email、passwordとpassword_confirmationが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
     it "nameがない場合は登録できないこと" do
       user = build(:user, name: nil)
       user.valid?
