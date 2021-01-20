6d60849a6231d157abdbdbe50debfc300634431f
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-20 15:05:39 +0900

    FactoryBotでテストを通した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-      user = User.new(name: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = FactoryBot.build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
     it "emailがない場合は登録できないこと" do
-      user = User.new(name: "manabu", email: nil, password: "00000000", password_confirmation: "00000000")
+      user = FactoryBot.build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
