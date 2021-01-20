b7e422a9fa21477a26ade118c36f811fcdf55762
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-20 16:18:47 +0900

    "passwordが6文字以上であれば登録できること"のテストを通した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,31 +5,19 @@ describe User do
       user = build(:user)
       expect(user).to be_valid
     end
-
+    
     it "nameがない場合は登録できないこと" do
       user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
-
+    
     it "emailがない場合は登録できないこと" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
-
-    it "passwordがない場合は登録できないこと" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
-    end
-
-    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
-    end
-
+    
     it "重複したemailが存在する場合登録できないこと" do
       #はじめにユーザーを登録
       user = create(:user)
@@ -38,5 +26,23 @@ describe User do
       another_user.valid?
       expect(another_user.errors[:email]).to include("has already been taken")
     end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      user = build(:user, password: "aaaaaa", password_confirmation: "aaaaaa")
+      expect(user).to be_valid
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
   end
 end
