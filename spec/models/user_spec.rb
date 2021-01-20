1540a5a858e10032e72352c8e6c8225c9aefa22b
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-20 15:08:16 +0900

    インスタンス生成時にFactoryBotを省略

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "nameがない場合は登録できないこと" do
-      user = FactoryBot.build(:user, name: nil)
+      user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
     it "emailがない場合は登録できないこと" do
-      user = FactoryBot.build(:user, email: nil)
+      user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
