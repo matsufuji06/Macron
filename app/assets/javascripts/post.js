fae3f1c93f8acefec002ee55b77aec7f059186ed
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-23 11:42:42 +0900

    ajaxメソッド、done、failを記述→エラー（doneが通らない）

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,7 +1,26 @@
 $(function() {
   $('.formGroup__submit .submitBtn').on('click', function(e) {
     e.preventDefault();
-    var foodList = $('.foodName .form').val();
+    const foodList = $('.foodName .form').val();
     console.log(foodList);
+    $.ajax({
+      type: 'POST',
+      url: '/posts',
+      data: {
+        foodList: {
+          food: foodList
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(data){
+      const foodHTML = $('td .foodResult').append(data.food);
+      $('tr').append(foodHTML);
+      $('.foodName .form').val('');
+    })
+    .fail(function() {
+      alert('error');
+    });
   });
+  
 });
