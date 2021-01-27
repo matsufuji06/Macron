e532839e63f2c6fa474f42af67b2d123dfdeb63f
matsufuji06 <n.matsufuji06@gmail.com>
2021-01-28 08:00:14 +0900

    createメソッドとプライベートメソッドを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comments_params)
+  end
+
+  private
+  def comments_params
+    params.require(:commnet).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
